dc9f20395c012a3fa99d38491bf685fb10362fef
Hamza-ye <7amza.it@gmail.com>
2025-03-13 03:05:33 +0300

    add native and web support

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -1,8 +1,11 @@
 import 'package:d_sdk/core/form/field_template/field_template.dart';
+import 'package:d_sdk/core/logging/new_app_logging.dart';
 import 'package:d_sdk/database/converters/converters.dart';
 import 'package:d_sdk/database/shared/shared.dart';
 import 'package:d_sdk/database/tables/tables.dart';
 import 'package:drift/drift.dart';
+import 'package:drift_flutter/drift_flutter.dart';
+import 'package:path_provider/path_provider.dart';
 
 part 'app_database.g.dart';
 
@@ -26,7 +29,28 @@ part 'app_database.g.dart';
   DataOptions
 ])
 class AppDatabase extends _$AppDatabase {
-  AppDatabase(QueryExecutor e) : super(e);
+  AppDatabase({QueryExecutor? e, String? databaseName})
+      : super(
+          e ??
+              driftDatabase(
+                name: databaseName ?? 'drun-db',
+                native: const DriftNativeOptions(
+                  databaseDirectory: getApplicationSupportDirectory,
+                ),
+                web: DriftWebOptions(
+                  sqlite3Wasm: Uri.parse('sqlite3.wasm'),
+                  driftWorker: Uri.parse('drift_worker.js'),
+                  onResult: (result) {
+                    if (result.missingFeatures.isNotEmpty) {
+                      logDebug(
+                        'Using ${result.chosenImplementation} due to unsupported '
+                        'browser features: ${result.missingFeatures}',
+                      );
+                    }
+                  },
+                ),
+              ),
+        );
 
   @override
   int get schemaVersion => 1;
