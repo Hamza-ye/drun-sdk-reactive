982dcddb1e69cc3fe4159bbc2f641372ff686bbf
Hamza-ye <7amza.it@gmail.com>
2025-04-07 21:26:31 +0300

    move progress event

--- lib/core/sync/model/sync_logger.dart
+++ lib/core/sync/model/sync_logger.dart
@@ -1,15 +1,15 @@
-import 'package:d_sdk/core/logging/new_app_logging.dart';
-import 'package:d_sdk/core/sync/model/sync_progress_event.dart';
-
-typedef ProgressCallback = void Function(SyncProgressEvent event);
-
-class SyncLogger {
-  final ProgressCallback progressCallback;
-
-  SyncLogger(this.progressCallback);
-
-  call(SyncProgressEvent syncProgressEvent) {
-    progressCallback.call(syncProgressEvent);
-    logDebug('sync progress', data: syncProgressEvent.toMap());
-  }
-}
+// import 'package:d_sdk/core/logging/new_app_logging.dart';
+// import 'package:d_sdk/core/sync/model/sync_progress_event.dart';
+//
+// typedef ProgressCallback = void Function(SyncProgressEvent event);
+//
+// class SyncLogger {
+//   final ProgressCallback progressCallback;
+//
+//   SyncLogger(this.progressCallback);
+//
+//   call(SyncProgressEvent syncProgressEvent) {
+//     progressCallback.call(syncProgressEvent);
+//     logDebug('sync progress', data: syncProgressEvent.toMap());
+//   }
+// }
--- lib/core/sync/model/sync_progress_event.dart
+++ lib/core/sync/model/sync_progress_event.dart
@@ -1,36 +1,36 @@
-enum SyncProgressState {
-  ENQUEUED,
-  RUNNING,
-  SUCCEEDED,
-  FAILED,
-  CANCELLED;
-
-  bool isFinished() {
-    return this == SUCCEEDED || this == FAILED || this == CANCELLED;
-  }
-}
-
-class SyncProgressEvent {
-  final String resourceName;
-  final SyncProgressState syncProgressState;
-  final String message;
-  final double percentage;
-  final bool completed;
-
-  const SyncProgressEvent({
-    required this.resourceName,
-    required this.syncProgressState,
-    required this.message,
-    required this.percentage,
-    this.completed = false,
-  });
-
-  Map<String, dynamic> toMap() {
-    return {
-      'resourceName': this.resourceName,
-      'syncProgressState': this.syncProgressState.name,
-      'percentage': this.percentage,
-      'completed': this.completed,
-    };
-  }
-}
+// enum SyncProgressState {
+//   ENQUEUED,
+//   RUNNING,
+//   SUCCEEDED,
+//   FAILED,
+//   CANCELLED;
+//
+//   bool isFinished() {
+//     return this == SUCCEEDED || this == FAILED || this == CANCELLED;
+//   }
+// }
+//
+// class SyncProgressEvent {
+//   final String resourceName;
+//   final SyncProgressState syncProgressState;
+//   final String message;
+//   final double percentage;
+//   final bool completed;
+//
+//   const SyncProgressEvent({
+//     required this.resourceName,
+//     required this.syncProgressState,
+//     required this.message,
+//     required this.percentage,
+//     this.completed = false,
+//   });
+//
+//   Map<String, dynamic> toMap() {
+//     return {
+//       'resourceName': this.resourceName,
+//       'syncProgressState': this.syncProgressState.name,
+//       'percentage': this.percentage,
+//       'completed': this.completed,
+//     };
+//   }
+// }
