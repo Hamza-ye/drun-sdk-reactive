715c8ee71fe3e6457b77ecde77969530ebe65a36
Hamza-ye <7amza.it@gmail.com>
2025-04-08 01:36:11 +0300

    add options field

--- lib/database/tables/data_option_sets.table.dart
+++ lib/database/tables/data_option_sets.table.dart
@@ -1,6 +1,7 @@
+import 'package:d_sdk/database/converters/form_option_list.converter.dart';
 import 'package:d_sdk/database/tables/tables.dart';
 import 'package:drift/drift.dart';
 
 class DataOptionSets extends Table with BaseTableMixin, IdentifiableMixin {
-  /// TextColumn options => text().map(const FormOptionListConverter()).nullable()();
+  TextColumn get options => text().map(const FormOptionListConverter()).nullable()();
 }
