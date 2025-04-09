2233899e4b171a2631d535997d8e4b7fdb4bbdf9
Hamza-ye <7amza.it@gmail.com>
2025-04-09 17:00:05 +0300

    remove cipher

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -1,12 +1,9 @@
 import 'package:d_sdk/core/form/field_template/field_template.dart';
-import 'package:d_sdk/core/logging/new_app_logging.dart';
 import 'package:d_sdk/database/converters/converters.dart';
 import 'package:d_sdk/database/dao/dao.dart';
 import 'package:d_sdk/database/shared/shared.dart';
 import 'package:d_sdk/database/tables/tables.dart';
 import 'package:drift/drift.dart';
-import 'package:drift_flutter/drift_flutter.dart';
-import 'package:path_provider/path_provider.dart';
 
 part 'app_database.g.dart';
 
@@ -40,9 +37,10 @@ part 'app_database.g.dart';
   UsersDao,
 ])
 class AppDatabase extends _$AppDatabase {
-  AppDatabase({QueryExecutor? executor, String? databaseName})
+  // AppDatabase(super.e, {String? databaseName});
+  AppDatabase({required QueryExecutor executor, String? databaseName})
       : super(
-          executor ??
+          executor /*??
 
               /// no encryption, called if QueryExecutor is null
               driftDatabase(
@@ -62,7 +60,7 @@ class AppDatabase extends _$AppDatabase {
                     }
                   },
                 ),
-              ),
+              ),*/
         );
 
   @override
