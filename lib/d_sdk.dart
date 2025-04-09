05f312d9bdd42a30459ee107567a79b6674746cd
Hamza-ye <7amza.it@gmail.com>
2025-04-10 02:32:44 +0300

    f

--- lib/d_sdk.dart
+++ lib/d_sdk.dart
@@ -19,53 +19,6 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class DSdk {
-  static SecureStorageAdapter _createSecureCacheStorageAdapter() =>
-      SecureStorageAdapter(cacheStorage: FlutterSecureStorage());
-
-  static SharedCacheStorageAdapter _createSharedCacheStorageAdapter(
-          SharedPreferences prefs) =>
-      SharedCacheStorageAdapter(cacheStorage: prefs);
-
-  static Future<SharedPreferences> get _prefs =>
-      SharedPreferences.getInstance();
-
-  // static CacheStorage _cachedStorage() => AppEnvironment.secureCache
-  //     ? createSharedCacheStorageAdapter(rSdkLocator<SharedPreferences>())
-  //     : createSecureCacheStorageAdapter();
-
-  static Dio _dio() => Dio(
-        BaseOptions(
-          baseUrl: AppEnvironment.apiBaseUrl,
-          sendTimeout: Duration(seconds: AppEnvironment.apiRequestSentTimeout),
-        ),
-      );
-
-  static Future<void> initialize(
-      {required String username, required String baseUrl}) async {
-    // if (rSdkLocator.isReadySync<DbManager>()) {
-    //   await rSdkLocator<DbManager>().closeDatabase();
-    //   rSdkLocator.reset();
-    // }
-
-    final prefs = await _prefs;
-
-    rSdkLocator.registerLazySingleton<SharedPreferences>(() => prefs);
-
-    rSdkLocator.registerSingleton<Dio>(_dio());
-    rSdkLocator.registerLazySingleton<CacheStorage>(() =>
-        AppEnvironment.secureCache
-            ? _createSharedCacheStorageAdapter(prefs)
-            : _createSecureCacheStorageAdapter());
-
-    rSdkLocator.registerSingleton<HttpClient<dynamic>>(
-        HttpAdapter(rSdkLocator<Dio>()),
-        instanceName: 'HttpAdapter');
-
-    // RunDatabaseConfig(databaseName: '${username}_$uri')
-    // Reinitialize the SDK with the new user database
-    // await setupSdkLocator(username: username, baseUrl: baseUrl);
-  }
-
   // database
   static DbManager get dbManager => rSdkLocator<DbManager>();
 
